7b8eef102b21165a36867f2478f94df1fd490a10
ilanus <hasanovtk@gmail.com>
2021-04-06 09:16:18 +0200

    update greeting message

--- src/utils/greeting.js
+++ src/utils/greeting.js
@@ -1,23 +1,14 @@
 export const getGreeting = () => {
-  const now = new Date();
-  const hours = now.getHours();
+  let greeting;
+  const time = new Date().getHours();
 
-  const greetings = [
-    {
-      tod: 'latenight',
-      greet: 'You`re still awake?',
-      test: hours >= 24 || hours <= 4,
-    },
-    { tod: 'night', greet: 'Good Night!', test: hours >= 21 || hours <= 11 },
-    { tod: 'evening', greet: 'Good Evening!', test: hours >= 18 && hours <= 20 },
-    { tod: 'afternoon', greet: 'Good Afternoon!', test: hours >= 13 && hours <= 17 },
-    { tod: 'noon', greet: 'Good Noon Visitor!', test: hours === 12 },
-    {
-      tod: 'morning',
-      greet: 'Good Morning!',
-      test: hours >= 5 && hours <= 11,
-    },
-  ];
+  if (time < 10) {
+    greeting = 'Good morning!';
+  } else if (time < 20) {
+    greeting = 'Good day!';
+  } else {
+    greeting = 'Good evening!';
+  }
 
-  return greetings.find(el => el.test).greet;
+  return greeting;
 };
